e817c74e8b5be0d43a7f531c85c4f20106782ce9
Vikas9130 <vikaspawar9130@gmail.com>
2024-01-03 10:25:11 +0530

    optimized code

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,35 +1,21 @@
 package com.example;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.TimePickerDialog;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TimePicker;
-
 import com.example.alarm.AlarmFragment;
-import com.example.alarm.AlarmReceiver;
-import com.example.alarm.BootUpReceiver;
 import com.example.databinding.ActivityMainBinding;
 import com.example.powernap.PowerNapFragment;
 
-import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity {
-    static final String TAG = "MainActivity";
+
     public static final int ALARM_REQ_CODE = 100;
 
     ActivityMainBinding mainXml;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         mainXml.btnPowerNap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Replace the fragment with PowerNapFragment
                 replaceFragment(new PowerNapFragment());
             }
         });
--- app/src/main/java/com/example/alarm/BootUpReceiver.java
+++ app/src/main/java/com/example/alarm/BootUpReceiver.java
@@ -1,5 +1,8 @@
 package com.example.alarm;
 
+/*
+this is made only for when device get rebooted.
+ */
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
